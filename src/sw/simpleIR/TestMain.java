ee8e9ea26efcbcc6f5a932fc4de62fbdd7e98327
0JIEUN0 <jieun4436@naver.com>
2021-03-11 10:51:14 +0900

    1st Ass : read html files and write parsed results in xml

---
+++ src/sw/simpleIR/TestMain.java
@@ -0,0 +1,11 @@
+package sw.simpleIR;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HttpCrawler crawler = new HttpCrawler();
+		crawler.crawlingHtmlToXml();
+	}
+
+}
